52f8a74a5b27968846b2695acddd786a842c7adf
BRobat <bartlomiej.robat@gmail.com>
2018-10-06 19:13:42 +0200

    Initial Commit

---
+++ CreatorsClub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CreatorsClub
+//
+//  Created by Bartłomiej Tabor on 06/10/2018.
+//  Copyright © 2018 Bartłomiej Tabor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
