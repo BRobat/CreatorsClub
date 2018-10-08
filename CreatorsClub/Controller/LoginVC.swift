ee0d99ef1448c961d05632ac773761c5353f4e1c
BRobat <bartlomiej.robat@gmail.com>
2018-10-08 09:34:12 +0200

    made basic architecture and added plist

---
+++ CreatorsClub/Controller/LoginVC.swift
@@ -0,0 +1,30 @@
+//
+//  LoginVC.swift
+//  CreatorsClub
+//
+//  Created by Bartłomiej Tabor on 07/10/2018.
+//  Copyright © 2018 Bartłomiej Tabor. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
